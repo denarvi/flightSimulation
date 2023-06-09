dda33ffa0d09f4f22749e91d12a2919a3d26a78b
Aravind <ardend@iu.edu>
2023-06-09 18:02:49 +0530

    basic visualisation

--- basic_rotation.py
+++ basic_rotation.py
@@ -1,16 +1,18 @@
 import tkinter as tk
-import math
+import numpy as np
+from math import pi
 
 # Set up some constants
 WIDTH, HEIGHT = 800, 600
 SPEED = 5
 ROT_SPEED = 0.02
 
-# Set up the cube
-cube_position = [WIDTH // 2, HEIGHT // 2, 0]
-cube_size = 100
-cube_rotation = [0, 0, 0]  # Pitch, yaw, roll
+# Set up the airplane
+airplane_position = [WIDTH // 2, HEIGHT // 2, 0]
+airplane_size = 100
+airplane_rotation = [0, 0, 0]  # Pitch, yaw, roll
 
+# Define the airplane's vertices
 vertices = [
     (0, 0, 2),  # Nose
     (-1, 1, -2), (1, 1, -2),  # Wings
@@ -26,7 +28,6 @@ edges = [
     (3, 6), (4, 5), (4, 6), (5, 6)  # Tail and inner wings
 ]
 
-
 # Create the window
 root = tk.Tk()
 
@@ -39,69 +40,69 @@ def rotate(point, angles):
     pitch, yaw, roll = angles
 
     # Rotate around x-axis (pitch)
-    y, z = y * math.cos(pitch) - z * math.sin(pitch), y * math.sin(pitch) + z * math.cos(pitch)
+    y, z = y * np.cos(pitch) - z * np.sin(pitch), y * np.sin(pitch) + z * np.cos(pitch)
 
     # Rotate around y-axis (yaw)
-    x, z = x * math.cos(yaw) + z * math.sin(yaw), -x * math.sin(yaw) + z * math.cos(yaw)
+    x, z = x * np.cos(yaw) + z * np.sin(yaw), -x * np.sin(yaw) + z * np.cos(yaw)
 
     # Rotate around z-axis (roll)
-    x, y = x * math.cos(roll) - y * math.sin(roll), x * math.sin(roll) + y * math.cos(roll)
+    x, y = x * np.cos(roll) - y * np.sin(roll), x * np.sin(roll) + y * np.cos(roll)
 
     return x, y, z
 
-def draw_cube():
+def draw_airplane():
     # Clear the canvas
     canvas.delete("all")
 
-    rotated_vertices = [rotate(vertex, cube_rotation) for vertex in vertices]
+    rotated_vertices = [rotate(vertex, airplane_rotation) for vertex in vertices]
 
     projected_points = []
     for x, y, z in rotated_vertices:
         # Project the 3D point to 2D
-        x3d = cube_size * x
-        y3d = cube_size * (y + cube_position[2] / cube_size)
-        z3d = cube_size * z
+        x3d = airplane_size * x
+        y3d = airplane_size * (y + airplane_position[2] / airplane_size)
+        z3d = airplane_size* z
         x2d = x3d / (4 - z3d / 200)
         y2d = y3d / (4 - z3d / 200)
 
-        projected_points.append((cube_position[0] + int(x2d), cube_position[1] + int(y2d)))
+        projected_points.append((airplane_position[0] + int(x2d), airplane_position[1] + int(y2d)))
 
     for edge in edges:
         canvas.create_line(projected_points[edge[0]], projected_points[edge[1]], fill="white")
 
     # Schedule the next update
-    root.after(1000 // 60, draw_cube)  # 60 FPS
+    root.after(1000 // 60, draw_airplane)  # 60 FPS
 
-def move_cube(event):
+def move_airplane(event):
     if event.keysym == "Left":
-        cube_position[0] -= SPEED
+        airplane_position[0] -= SPEED
     if event.keysym == "Right":
-        cube_position[0] += SPEED
+        airplane_position[0] += SPEED
     if event.keysym == "Up":
-        cube_position[1] -= SPEED
+        airplane_position[1] -= SPEED
     if event.keysym == "Down":
-        cube_position[1] += SPEED
+        airplane_position[1] += SPEED
     if event.char == "a":
-        cube_position[2] -= SPEED
+        airplane_position[2] -= SPEED
     if event.char == "d":
-        cube_position[2] += SPEED
-    if event.char == "w": #s
-        cube_rotation[0] += ROT_SPEED  # Pitch
-    if event.char == "s": #w
-        cube_rotation[0] -= ROT_SPEED  # Pitch
-    if event.char == "a": #a
-        cube_rotation[1] += ROT_SPEED  # Yaw
-    if event.char == "d": #d
-        cube_rotation[1] -= ROT_SPEED  # Yaw
+        airplane_position[2] += SPEED
+    if event.char == "q":
+        airplane_rotation[0] += ROT_SPEED  # Pitch
+    if event.char == "w":
+        airplane_rotation[0] -= ROT_SPEED  # Pitch
+    if event.char == "e":
+        airplane_rotation[1] += ROT_SPEED  # Yaw
+    if event.char == "r":
+        airplane_rotation[1] -= ROT_SPEED  # Yaw
     if event.char == "t":
-        cube_rotation[2] += ROT_SPEED  # Roll
+        airplane_rotation[2] += ROT_SPEED  # Roll
     if event.char == "y":
-        cube_rotation[2] -= ROT_SPEED  # Roll
+        airplane_rotation[2] -= ROT_SPEED  # Roll
 
 # Bind the keyboard events
-root.bind("<Key>", move_cube)
+root.bind("<Key>", move_airplane)
 
 # Start the game loop
-draw_cube()
+draw_airplane()
 
 root.mainloop()
